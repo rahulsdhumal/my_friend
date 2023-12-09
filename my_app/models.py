788d3458ee0376611eeeb2feec7aa85a1a0d29f4
rahuldhumal <rahuldhumal208@gmail.com>
2023-12-09 17:55:58 +0530

    update model

--- my_app/models.py
+++ my_app/models.py
@@ -1,23 +1,47 @@
 from django.db import models
 
 # Create your models here.
-class friends(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    date_of_birth = models.DateField(null=True)
+class friend_info(models.Model):
+    first_name = models.CharField(max_length=100,null=False)
+    last_name = models.CharField(max_length=100,null=False)
+    contact = models.BigIntegerField(null=False)
+    alternative_contact = models.BigIntegerField(null=True)
+    email = models.EmailField(null=False)
+    date_of_birth = models.DateField(null=False)
+    address = models.CharField(max_length=200,null=False)
+    maritial_status = models.BooleanField(null=False)
+    favorite_sports = models.CharField(max_length=200,null=True)
+    hobbies = models.CharField(max_length=200,null=True)
+
 
 class location(models.Model):
-    name = models.CharField(max_length=100,null=True)
+    name = models.CharField(max_length=100,null=False)
 
-class company_details(models.Model):
-    friend = models.ForeignKey(friends,on_delete=models.CASCADE)
-    current_company = models.CharField(max_length=50)
+class current_company_info(models.Model):
+    company_name = models.CharField(max_length=50,null=False)
     location = models.ForeignKey(location,on_delete=models.CASCADE)
-    position = models.CharField(max_length=100,null=True)
-    date_of_joining = models.DateField(null=True)
+    date_of_joining = models.DateField(null=False)
+    last_day_date = models.DateField(null=True)
+    position = models.CharField(max_length=100,null=False)
+    friend = models.ForeignKey(friend_info,on_delete=models.CASCADE)
+    grade_out_of_10 = models.IntegerField(null=True)
 
+class old_company_info(models.Model):
+    company_name = models.CharField(max_length=50,null=False)
+    location = models.ForeignKey(location,on_delete=models.CASCADE)
+    date_of_joining = models.DateField(null=False)
+    last_day_date = models.DateField(null=False)
+    position = models.CharField(max_length=100,null=False)
+    friend = models.ForeignKey(friend_info,on_delete=models.CASCADE)
+    grade_out_of_10 = models.IntegerField(null=True)
+    reason_for_switch = models.CharField(max_length=500,null=True)
 
+class biodata(models.Model):
+    resume = models.FileField(null=False)
+    friend = models.ForeignKey(friend_info,on_delete=models.CASCADE)
 
-
-
-
+class count_total_exp(models.Model):
+    friend = models.ForeignKey(friend_info,on_delete=models.CASCADE)
+    current_company = models.ForeignKey(current_company_info,on_delete=models.CASCADE)
+    old_company = models.ForeignKey(old_company_info,on_delete=models.CASCADE)
+    total_exp = models.CharField(max_length=200,null=False)
--- my_app/views.py
+++ my_app/views.py
@@ -15,12 +15,26 @@ class FriendsDetailsView(generics.ListAPIView):
             data = request.data
             first_name = data["first_name"]
             last_name = data["last_name"]
+            contact = data["contact"]
+            alternative_contact = data["alternative_contact"]
+            email = data["email"]
             date_of_birth = data["date_of_birth"]
-
-            bestfriends_obj = friends(
+            address = data["address"]
+            maritial_status = data["maritial_status"]
+            favorite_sports = data["favorite_sports"]
+            hobbies = data["hobbies"]
+    
+            bestfriends_obj = friend_info(
                 first_name = first_name,
                 last_name = last_name,
-                date_of_birth = date_of_birth
+                contact = contact,
+                alternative_contact = alternative_contact,
+                email = email,
+                date_of_birth = date_of_birth,
+                address = address,
+                maritial_status = maritial_status,
+                favorite_sports = favorite_sports,
+                hobbies = hobbies
             )
 
             bestfriends_obj.save()
@@ -41,7 +55,7 @@ class FriendsDetailsView(generics.ListAPIView):
     def get(self,request):
         try:
             response_data = []
-            friends_data = friends.objects.all()
+            friends_data = friend_info.objects.all()
 
             for i in friends_data:
                 full_name = str(f"{i.first_name} {i.last_name}")
@@ -51,8 +65,14 @@ class FriendsDetailsView(generics.ListAPIView):
                 response_data.append({
                     "id":i.pk,
                     "name":full_name,
+                    "contact_number":i.contact,
+                    "alternative_contact":i.alternative_contact,
+                    "email":i.email,
                     "date_of_birth":str(i.date_of_birth),
-                    "age":f"{years} years"
+                    "age":f"{years} years",
+                    "address":i.address,
+                    "maritial_status":i.maritial_status,
+                    "hobbies":i.hobbies
                 })
 
             return HttpResponse(
@@ -119,106 +139,106 @@ class LocationDetailsView(generics.ListAPIView):
                 content_type="application/json",
             )
 
-class CompanyDetailsView(generics.ListAPIView):
-    def post(self,request):
-        try:
-            data = request.data
-            friend_id = data["friend_id"]
-            current_company = data["current_company"]
-            location_id = data["location_id"]
-            position = data["position"]
-            date_of_joining = data["date_of_joining"]
+# class CompanyDetailsView(generics.ListAPIView):
+#     def post(self,request):
+#         try:
+#             data = request.data
+#             friend_id = data["friend_id"]
+#             current_company = data["current_company"]
+#             location_id = data["location_id"]
+#             position = data["position"]
+#             date_of_joining = data["date_of_joining"]
 
-            bestfriends_obj = company_details(
-                friend_id = friend_id,
-                current_company = current_company,
-                location_id = location_id,
-                position = position,
-                date_of_joining = date_of_joining
-            )
+#             bestfriends_obj = company_details(
+#                 friend_id = friend_id,
+#                 current_company = current_company,
+#                 location_id = location_id,
+#                 position = position,
+#                 date_of_joining = date_of_joining
+#             )
 
-            bestfriends_obj.save()
-            return HttpResponse(
-                json.dumps({"status":"success","message":"Company details added successfully"}),
-                status=200,
-                content_type="application/json",
-            )
+#             bestfriends_obj.save()
+#             return HttpResponse(
+#                 json.dumps({"status":"success","message":"Company details added successfully"}),
+#                 status=200,
+#                 content_type="application/json",
+#             )
         
-        except Exception as ex:
-            traceback.print_exc()
-            return HttpResponse(
-                json.dumps({"status":"failed","message":"There is backed issue, please contact administrator"}),
-                status=200,
-                content_type="application/json",
-            )
+#         except Exception as ex:
+#             traceback.print_exc()
+#             return HttpResponse(
+#                 json.dumps({"status":"failed","message":"There is backed issue, please contact administrator"}),
+#                 status=200,
+#                 content_type="application/json",
+#             )
         
-    def get(self,request):
-        try:
-            response_data = []
-            friends_data = company_details.objects.all()
+#     def get(self,request):
+#         try:
+#             response_data = []
+#             friends_data = company_details.objects.all()
 
-            for i in friends_data:
-                full_name = str(f"{i.friend.first_name} {i.friend.last_name}")
-                date_of_joining = datetime(i.date_of_joining.year, i.date_of_joining.month, i.date_of_joining.day)
-                difference = datetime.now() - date_of_joining
-                years = difference.days // 365
-                months = (difference.days % 365) // 30
-                days = (difference.days % 365) % 30
-                response_data.append({
-                    "id":i.pk,
-                    "name":full_name,
-                    "current_company":i.current_company,
-                    "location":i.location.name,
-                    "position":i.position,
-                    "date_of_joining":str(date_of_joining.date()),
-                    "Total Experiance":f"{years} years, {months} months, {days} days"
+#             for i in friends_data:
+#                 full_name = str(f"{i.friend.first_name} {i.friend.last_name}")
+#                 date_of_joining = datetime(i.date_of_joining.year, i.date_of_joining.month, i.date_of_joining.day)
+#                 difference = datetime.now() - date_of_joining
+#                 years = difference.days // 365
+#                 months = (difference.days % 365) // 30
+#                 days = (difference.days % 365) % 30
+#                 response_data.append({
+#                     "id":i.pk,
+#                     "name":full_name,
+#                     "current_company":i.current_company,
+#                     "location":i.location.name,
+#                     "position":i.position,
+#                     "date_of_joining":str(date_of_joining.date()),
+#                     "Total Experiance":f"{years} years, {months} months, {days} days"
 
-                })
+#                 })
 
-            return HttpResponse(
-                json.dumps({"status":"success","message":"Company details retrived successfully","data":response_data}),
-                status=200,
-                content_type="application/json",
-            )
+#             return HttpResponse(
+#                 json.dumps({"status":"success","message":"Company details retrived successfully","data":response_data}),
+#                 status=200,
+#                 content_type="application/json",
+#             )
         
-        except Exception as ex:
-            traceback.print_exc()
-            return HttpResponse(
-                json.dumps({"status":"failed","message":"There is backed issue, please contact administrator"}),
-                status=200,
-                content_type="application/json",
-            )
+#         except Exception as ex:
+#             traceback.print_exc()
+#             return HttpResponse(
+#                 json.dumps({"status":"failed","message":"There is backed issue, please contact administrator"}),
+#                 status=200,
+#                 content_type="application/json",
+#             )
 
-class DeleteFriendDetailsView(generics.ListAPIView):
-    def post(self,request):
-        try:
-            data = request.data
-            friend_id = data.get("id")
+# class DeleteFriendDetailsView(generics.ListAPIView):
+#     def post(self,request):
+#         try:
+#             data = request.data
+#             friend_id = data.get("id")
 
-            # Check if the ID exists in the table
-            try:
-                friend = friends.objects.get(id=friend_id)
-            except ObjectDoesNotExist:
-                return HttpResponse(
-                    json.dumps({"status": "failed", "message": "Friend ID does not exist"}),
-                    status=404,
-                    content_type="application/json",
-                )
+#             # Check if the ID exists in the table
+#             try:
+#                 friend = friends.objects.get(id=friend_id)
+#             except ObjectDoesNotExist:
+#                 return HttpResponse(
+#                     json.dumps({"status": "failed", "message": "Friend ID does not exist"}),
+#                     status=404,
+#                     content_type="application/json",
+#                 )
 
-            # If the ID exists, proceed with deletion
-            with connection.cursor() as cursor:
-                cursor.execute(f"DELETE FROM my_app_bestfriends WHERE id = {friend_id}")
+#             # If the ID exists, proceed with deletion
+#             with connection.cursor() as cursor:
+#                 cursor.execute(f"DELETE FROM my_app_bestfriends WHERE id = {friend_id}")
 
-            return HttpResponse(
-                json.dumps({"status":"success","message":"Friend details deleted successfully"}),
-                status=200,
-                content_type="application/json",
-            )
+#             return HttpResponse(
+#                 json.dumps({"status":"success","message":"Friend details deleted successfully"}),
+#                 status=200,
+#                 content_type="application/json",
+#             )
         
-        except Exception as ex:
-            traceback.print_exc()
-            return HttpResponse(
-                json.dumps({"status":"failed","message":"There is backed issue, please contact administrator"}),
-                status=200,
-                content_type="application/json",
-            )
+#         except Exception as ex:
+#             traceback.print_exc()
+#             return HttpResponse(
+#                 json.dumps({"status":"failed","message":"There is backed issue, please contact administrator"}),
+#                 status=200,
+#                 content_type="application/json",
+#             )
